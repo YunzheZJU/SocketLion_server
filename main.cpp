60582a2871058a013542e733f9eafcc63c0c9969
Yunzhe <iLoveLive@honoka.moe>
2017-12-25 09:02:58 +0800

    Add Winsock2.h.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
+#include <Winsock2.h>
 #include <iostream>
 
 int main() {
+    WORD wVersionRequested;
+    WSADATA wsaData;
+    int err;
+
+    wVersionRequested = MAKEWORD( 1, 1 );
+
+    err = WSAStartup( wVersionRequested, &wsaData );//该函数的功能是加载一个Winsocket库版本
+    if ( err != 0 ) {
+        return 1;
+    }
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
