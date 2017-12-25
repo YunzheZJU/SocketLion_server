c8a6fc79842041e770cf55182f95e9d0a861f830
Yunzhe <iLoveLive@honoka.moe>
2017-12-25 12:05:14 +0800

    Add Creating socket.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,35 @@
 #include <Winsock2.h>
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    WORD wVersionRequested;
-    WSADATA wsaData;
+    cout << "Start." << endl;
+    WORD wVersionRequested = MAKEWORD(2, 2);
+    WSADATA wsaData{};
     int err;
 
-    wVersionRequested = MAKEWORD( 1, 1 );
-
-    err = WSAStartup( wVersionRequested, &wsaData );//该函数的功能是加载一个Winsocket库版本
-    if ( err != 0 ) {
+    // Load winsocket dll
+    err = WSAStartup(wVersionRequested, &wsaData);
+    if (err != 0) {
+        cout << "Error occurred in initialization: " << WSAGetLastError() << endl;
         return 1;
+    } else {
+        // Check the lowest and highest byte of the version in HEX
+        if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2) {
+            cout << "Could not find a usable version of Winsock.dll." << endl;
+            WSACleanup();
+            return 1;
+        } else {
+            cout << "The Winsock 2.2 dll was found successfully." << endl;
+        }
     }
-    std::cout << "Hello, World!" << std::endl;
+
+    // Create socket based on TCP
+    SOCKET socket = ::socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+
+    cout << "Bye." << endl;
+    WSACleanup();
     return 0;
 }
